84cd2036767bb8016c7f31b5ee549d084b2d0bf7
Gustavo Pukanski <pukanski@hotmail.com>
2025-11-05 19:43:51 -0300

    Classe Veiculo

---
+++ Veiculo.java
@@ -0,0 +1,15 @@
+class Veiculo{
+    private int velocidade;
+
+    public Veiculo(int velocidade){
+        this.velocidade = velocidade;
+    }
+
+    public int getVelocidade(){
+        return this.velocidade;
+    }
+
+    public void setVelocidade(int velocidade){
+        this.velocidade = velocidade;
+    }
+}
